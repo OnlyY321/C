7ebfb87544c5b41cae8f23dbdf094667e9e2167a
OnlyY321 <onlyy.dev.321@gmail.com>
2025-03-12 17:32:09 +0900

    first commit

---
+++ Dividend.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int divide_p(int dividend, int divisor, int *quotient, int *rem)
+{
+    if (divisor == 0)
+    {
+        return -1;
+    }
+    *quotient = dividend / divisor;
+    *rem = dividend % divisor;
+}
+
+int main()
+{
+    int i, j, q, r;
+    printf("enter dividend\n");
+    scanf("%d", &i);
+    printf("enter divisor\n");
+    scanf("%d", &j);
+    if (divide_p(i, j, &q, &r) == -1)
+        printf("0으로 나눌 수 없습니다.\n");
+    else
+        printf("%d / %d : 몫은 %d이고 나머지는 %d입니다.\n", i, j, q, r);
+}
